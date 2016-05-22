1e2e29db7fcfb829465d1ff26fbe7deda5967c1c
chinskiy <chinskiy93@gmail.com>
2016-05-22 23:31:08 +0300

    new api descr -> many changes

--- bl/processing.py
+++ bl/processing.py
@@ -1,10 +1,13 @@
 import pika
 import json
 import pymongo
+import ConfigParser
+import pymongo.errors as pmgerr
 
 # pika settings
 connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
 channel = connection.channel()
+channel.exchange_declare(exchange='tornado', type='topic', durable=True)
 
 channel.queue_declare(queue="creation", durable=True)
 channel.queue_declare(queue="reading", durable=True)
@@ -15,27 +18,33 @@ channel.queue_declare(queue="answer", durable=True)
 
 channel.basic_qos(prefetch_count=1)  # count messages to a worker at a time
 
+
+#replica connection string
+cfg_parser = ConfigParser.ConfigParser()
+cfg_parser.read('mongo_conf.ini')
+
+cfg = ''
+for _ in cfg_parser.sections():
+    cfg = cfg + cfg_parser.get(_, 'ip') + ':'
+    cfg = cfg + cfg_parser.get(_, 'port') + ','
+cfg = 'mongodb://' + cfg[:-1]
+
 # pymongo settings
-client = pymongo.MongoClient("172.17.0.3", 27017)
+client = pymongo.MongoClient(cfg)
 db = client["local"]["test"]
 
 
 def callback_creation(ch, method, properties, body):
     body = json.loads(body)
-    if len(body["id"]) == 0 or len(body["message"]) == 0:
-        answer = {"status": 400, "message": "Enter id or/and message"}
+    if len(body["id"]) == 0:
+        answer = {"status": 400, "response": "Id was missing"}
     else:
         try:
-            if db.find_one({"id": body["id"]}) is None:
-                db.insert_one(body)
-            else:
-                db.update_one({"id": body["id"]}, {"$set":
-                              {"message": body["message"]}})
-            answer = {"status": 200, "message": "Message created or modified"}
-        except pymongo.errors.ServerSelectionTimeoutError, \
-                pymongo.errors.NetworkTimeout:
-            answer = {"status": 400, "message": "Something gone wrong"}
-    channel.basic_publish(exchange="",
+            db.insert_one(body)
+            answer = {"status": 200, "response": "Message was added"}
+        except pmgerr.ServerSelectionTimeoutError, pmgerr.NetworkTimeout:
+            answer = {"status": 500, "response": "Something has gone wrong"}
+    channel.basic_publish(exchange="tornado",
                           routing_key="answer",
                           body=json.dumps(answer),
                           properties=pika.BasicProperties(delivery_mode=2,))
@@ -45,18 +54,18 @@ def callback_creation(ch, method, properties, body):
 def callback_reading(ch, method, properties, body):
     body = json.loads(body)
     if len(body["id"]) == 0:
-        answer = {"status": 400, "message": "Enter id"}
+        answer = {"status": 400, "response": "Id was missing"}
     else:
         try:
             answer = db.find_one(body)
-        except pymongo.errors.ServerSelectionTimeoutError, \
-                pymongo.errors.NetworkTimeout:
-            answer = {"status": 400, "message": "Something gone wrong"}
+        except pmgerr.ServerSelectionTimeoutError, pmgerr.NetworkTimeout:
+            answer = {"status": 500, "response": "Something has gone wrong"}
     if answer is None:
-        answer = {"status": 400, "message": "Message not exist"}
+        db.insert_one({"id": body["id"], "content": ""})
+        answer = {"status": 200, "content": ""}
     else:
-        answer = answer["message"]
-    channel.basic_publish(exchange="",
+        answer = {"status": 200, "content": answer["content"]}
+    channel.basic_publish(exchange="tornado",
                           routing_key="answer",
                           body=json.dumps(answer),
                           properties=pika.BasicProperties(delivery_mode=2,))
@@ -70,7 +79,7 @@ channel.basic_consume(callback_reading, queue="reading")
 # TEST
 def callback_test(ch, method, properties, body):
     body = json.loads(body)
-    print(" [x] Received %r" % body)
+    print("Received %r" % body)
     ch.basic_ack(delivery_tag=method.delivery_tag)
 channel.basic_consume(callback_test, queue="answer")
 # TEST
