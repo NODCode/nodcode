2ffdc283dc79735dba33364fa7b91f6ac23c8d14
Kateryna_Mishyna <Kateryna_Mishyna@epam.com>
2016-05-25 21:06:40 +0300

    Add redirect to another server into client part

--- client/src/js/services.js
+++ client/src/js/services.js
@@ -1,41 +1,91 @@
 (function() {
     angular.module('NodeCode')
+        .config(['$httpProvider', function($httpProvider) {
+            $httpProvider.defaults.useXDomain = true;
+            delete $httpProvider.defaults.headers.common['X-Requested-With'];
+        }])
         .factory('nodService', function($http, $q, $httpParamSerializer) {
             var nod = {};
-            var url = 'http://localhost:8080/';
+            var url = ['http://localhost:8080/', 'http://localhost:8070/', 'http://localhost:8060/'];
             var options = {
-              headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
             };
 
             nod.sendMessage = function(idArg, messageArg) {
                 var deferred = $q.defer();
-                $http.post(url,
-                           $httpParamSerializer({'id': idArg, 'message': messageArg}),
-                           options)
+                $http.post(url[0],
+                        $httpParamSerializer({ 'id': idArg, 'message': messageArg }),
+                        options)
                     .then(function(data) {
                         deferred.resolve(data);
-                        console.log("ok");
+                        console.log("OK SEND", data);
                     }, function(err) {
-                        console.log("err", err);
-                        deferred.reject();
+                        console.log("err SEND", err);
+                        // deferred.reject();
+                        $http.post(url[1],
+                                $httpParamSerializer({ 'id': idArg, 'message': messageArg }),
+                                options)
+                            .then(function(data) {
+                                deferred.resolve(data);
+                                console.log("OK SEND", data);
+                            }, function(err) {
+                                console.log("err SEND", err);
+                                $http.post(url[2],
+                                        $httpParamSerializer({ 'id': idArg, 'message': messageArg }),
+                                        options)
+                                    .then(function(data) {
+                                        deferred.resolve(data);
+                                        console.log("OK SEND", data);
+                                    }, function(err) {
+                                        console.log("err SEND", err);
+                                        deferred.reject();
+
+                                        alert("WITHOUT CHANCE TO CONNECT TO SERVER");
+
+                                    });
+
+                            });
+
                     });
+                console.log("PROMISE", deferred.promise);
 
                 return deferred.promise;
             };
             nod.getMessage = function(idArg) {
                 var deferred = $q.defer();
 
-                $http.post(url,
-                           $httpParamSerializer({'id': idArg}),
-                           options)
+                $http.post(url[0],
+                        $httpParamSerializer({ 'id': idArg }),
+                        options)
                     .then(function(data) {
                         deferred.resolve(data);
-                        console.log("ok");
+                        console.log("OK GET", data);
                     }, function(err) {
-                        console.log("err", err);
-                        deferred.reject();
-                    });
+                        console.log("err GET", err);
 
+                        $http.post(url[1],
+                                $httpParamSerializer({ 'id': idArg }),
+                                options)
+                            .then(function(data) {
+                                deferred.resolve(data);
+                                console.log("OK GET", data);
+                            }, function(err) {
+                                console.log("err GET", err);
+
+                                $http.post(url[2],
+                                        $httpParamSerializer({ 'id': idArg }),
+                                        options)
+                                    .then(function(data) {
+                                        deferred.resolve(data);
+                                        console.log("OK GET", data);
+                                    }, function(err) {
+                                        console.log("err GET", err);
+                                        deferred.reject();
+                                        alert("WITHOUT CHANCE TO CONNECT TO SERVER");
+                                    });
+                            });
+                    });
+                console.log("PROMISE", deferred.promise);
                 return deferred.promise;
             };
 
